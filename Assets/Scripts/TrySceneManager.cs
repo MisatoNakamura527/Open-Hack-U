be226b2795ed0f667f694d6106d6a0983ab2e14b
MisatoNakamura527 <misato.527527@asagi.waseda.jp>
2020-08-22 00:16:59 +0900

    Add back button
    add  Screen transition. but back button ONLY returns to the title.

---
+++ Assets/Scripts/TrySceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TrySceneManager : MonoBehaviour
+{
+    
+    [SerializeField] Button backButton;
+
+    string nextscene;
+    [SerializeField] string titlescenename;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backButton.onClick.AddListener(() =>
+       {
+            nextscene = titlescenename;
+            ChangeScene();
+       });
+
+    }
+
+    void ChangeScene(){
+        SceneManager.LoadScene(nextscene);
+    }
+
+    void OnDestroy()
+   {
+    //    alertButton.onClick.RemoveAllListeners();
+   }
+
+}
