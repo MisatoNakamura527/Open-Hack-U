a4fa3c54c1992f67e961b498930768e936b92b50
MisatoNakamura527 <misato.527527@asagi.waseda.jp>
2020-08-21 22:59:44 +0900

    Initialize repo
    Init

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] Button designButton;
+    [SerializeField] Button registeredButton;
+    [SerializeField] Button nailButton;
+
+    string nextscene;
+
+    [SerializeField] string designscene;
+    [SerializeField] string registeredscene;
+    [SerializeField] string nailscene;
+
+
+
+   void Start()
+   {
+       designButton.onClick.AddListener(() =>
+       {
+            nextscene = designscene;
+            ChangeScene();
+       });
+
+       registeredButton.onClick.AddListener(() =>
+       {
+            nextscene = registeredscene;
+            ChangeScene();
+       });
+
+       nailButton.onClick.AddListener(() =>
+       {
+           nextscene = nailscene;
+            ChangeScene();
+       });
+   }
+
+    void ChangeScene(){
+        SceneManager.LoadScene(nextscene);
+    }
+   void OnDestroy()
+   {
+    //    alertButton.onClick.RemoveAllListeners();
+   }
+}
