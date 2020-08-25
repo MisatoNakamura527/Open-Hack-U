4906866eb7238c33c526debe3cdbd307c1e8790b
MisatoNakamura527 <misato.527527@asagi.waseda.jp>
2020-08-26 00:41:58 +0900

    put on plane
    put plane for paint

--- Assets/Scripts/NailDesign.cs
+++ Assets/Scripts/NailDesign.cs
@@ -1,177 +1,90 @@
 ﻿using UnityEngine;
-using System;
-using System.Linq;
+using System.Collections;
 using System.IO;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class NailDesign : MonoBehaviour
 {
-    Texture2D texture;　// キャンパス
-    Vector3 beforeMousePos;
+    Texture2D drawTexture;
+    Color[] buffer;
+    bool touching = false;
+    Vector2 prevPoint;
 
-    Color bgColor = Color.white;
-    Color lineColor = Color.black;
 
     void Start()
     {
-        var img = GetComponent<RawImage>();
-        var rt = GetComponent<RectTransform>();
-        var width = (int)rt.rect.width;
-        var height = (int)rt.rect.height;
-        // texture = ReadTexture("Assets/Materials/nail_plane.jpg", 600, 600);
-        texture = ReadPng("Assets/Materials/nail_plane.png");
+        Texture2D mainTexture = (Texture2D)GetComponent<Renderer>().material.mainTexture;
+        if(mainTexture == null){
+            Debug.Log("null");
+        }else{
+            Debug.Log(mainTexture.height);
+        }
 
-        // texture = new Texture2D(width, height, TextureFormat.ARGB32, false);
-        // img.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
-        // img.texture = texture;
+        Color[] pixels = mainTexture.GetPixels();
 
-        //背景が透明なTexture2Dを作る
-        //http://d.hatena.ne.jp/shinriyo/20140520/p2
-        // Color32[] texColors = Enumerable.Repeat<Color32>(bgColor, width * height).ToArray();
-        // texture.SetPixels32(texColors);
+        buffer = new Color[pixels.Length];
+        pixels.CopyTo(buffer, 0);
 
-        texture.Apply();
+        drawTexture = new Texture2D(mainTexture.width, mainTexture.height, TextureFormat.RGBA32, false);
+        drawTexture.filterMode = FilterMode.Point;
+    }
+
+    public void DrawLine(Vector2 p, Vector2 q)
+    {
+        var lerpNum = 10;
+        for(int i=0; i < lerpNum + 1; i++)
+        {
+            var r = Vector2.Lerp(p, q, i * (1.0f / lerpNum));
+            Draw(r);
+        }
+    }
+
+    public void Draw(Vector2 p)
+    {
+        p.x = (int)p.x;
+        p.y = (int)p.y;
+
+        var brushSize = 5;
+        var color = Color.black;
+        for (int x = Mathf.Max(0, (int)(p.x - brushSize-1)); x < Mathf.Min(drawTexture.width, (int)(p.x + brushSize+1)); x++)
+        {
+            for (int y = Mathf.Max(0, (int)(p.y - brushSize-1)); y < Mathf.Min(drawTexture.height, (int)(p.y + brushSize+1)); y++)
+            {
+                if (Mathf.Pow(p.x - x,2) + Mathf.Pow(p.y -y, 2) < Mathf.Pow(brushSize, 2))
+                {
+                    buffer.SetValue(color, x + drawTexture.width * y);
+                }
+            }
+        }
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            beforeMousePos = GetPosition();
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            Vector3 v = GetPosition();
-            LineTo(beforeMousePos, v, lineColor);
-            beforeMousePos = v;
-            texture.Apply();
-        }
-    }
-
-    /// <summary>
-    /// UIのクリック座標のx、y座標を求める - 新しいguiシステムの画像 - Unity Answers
-    /// https://answers.unity.com/questions/892333/find-xy-cordinates-of-click-on-uiimage-new-gui-sys.html
-    /// </summary>
-    public Vector3 GetPosition()
-    {
-        var dat = new PointerEventData(EventSystem.current);
-        dat.position = Input.mousePosition;
-
-        var rect1 = GetComponent<RectTransform>();
-        var pos1 = dat.position;
-        if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(rect1, pos1,
-            null, out Vector2 localCursor))
-            return localCursor;
-
-        int xpos = (int)(localCursor.x);
-        int ypos = (int)(localCursor.y);
-
-        if (xpos < 0) xpos = xpos + (int)rect1.rect.width / 2;
-        else xpos += (int)rect1.rect.width / 2;
-
-        if (ypos > 0) ypos = ypos + (int)rect1.rect.height / 2;
-        else ypos += (int)rect1.rect.height / 2;
-
-        // Debug.Log("Correct Cursor Pos: " + xpos + " " + ypos);
-        return new Vector3(xpos, ypos, 0);
-    }
-
-    /// Unityでお絵描きしてみる
-    /// http://tech.gmo-media.jp/post/56101930112/draw-a-picture-with-unity
-
-
-    public void LineTo(Vector3 start, Vector3 end, Color color)
-    {
-        float x = start.x, y = start.y;
-        // color of pixels
-        Color[] wcolor = { color };
-
-        if (Mathf.Abs(start.x - end.x) > Mathf.Abs(start.y - end.y))
-        {
-            float dy = Math.Abs(end.x - start.x) < float.Epsilon ? 0 : (end.y - start.y) / (end.x - start.x);
-            float dx = start.x < end.x ? 1 : -1;
-            //draw line loop
-            while (x > 0 && x < texture.width && y > 0 && y < texture.height)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f))
             {
-                try
-                {
-                    texture.SetPixels((int)x, (int)y, 1, 1, wcolor);
-                    x += dx;
-                    y += dx * dy;
-                    if (start.x < end.x && x > end.x ||
-                        start.x > end.x && x < end.x)
-                    {
-                        break;
-                    }
+                var drawPoint = new Vector2(hit.textureCoord.x * drawTexture.width, hit.textureCoord.y * drawTexture.height);
+                if (touching) {
+                    DrawLine(prevPoint, drawPoint);
                 }
-                catch (Exception e)
+                else
                 {
-                    Debug.LogException(e);
-                    break;
+                    Draw(drawPoint);
                 }
-            }
-        }
-        else if (Mathf.Abs(start.x - end.x) < Mathf.Abs(start.y - end.y))
-        {
-            float dx = Math.Abs(start.y - end.y) < float.Epsilon ? 0 : (end.x - start.x) / (end.y - start.y);
-            float dy = start.y < end.y ? 1 : -1;
-            while (x > 0 && x < texture.width && y > 0 && y < texture.height)
+                prevPoint = drawPoint;
+                touching = true;
+            }else
             {
-                try
-                {
-                    texture.SetPixels((int)x, (int)y, 1, 1, wcolor);
-                    x += dx * dy;
-                    y += dy;
-                    if (start.y < end.y && y > end.y ||
-                        start.y > end.y && y < end.y)
-                    {
-                        break;
-                    }
-                }
-                catch (Exception e)
-                {
-                    Debug.LogException(e);
-                    break;
-                }
+                touching = false;
             }
-        }
-    }
-
-    byte[] ReadPngFile(string path){
-        Debug.Log("read start");
-
-        FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
-        BinaryReader bin = new BinaryReader(fileStream);
-        byte[] values = bin.ReadBytes((int)bin.BaseStream.Length);
-
-        bin.Close();
-
-        return values;
-    }
-
-     Texture2D ReadPng(string path)
-    {
-        byte[] readBinary = ReadPngFile(path);
-        Debug.Log("here");
-
-        int pos = 16; // 16バイトから開始
-
-        int width = 0;
-        for (int i = 0; i < 4; i++)
+            drawTexture.SetPixels(buffer);
+            drawTexture.Apply();
+            GetComponent<Renderer>().material.mainTexture = drawTexture;
+        }else
         {
-            width = width * 256 + readBinary[pos++];
+            touching = false;
         }
-
-        int height = 0;
-        for (int i = 0; i < 4; i++)
-        {
-            height = height * 256 + readBinary[pos++];
-        }
-
-        Texture2D texture = new Texture2D(width, height);
-        texture.LoadImage(readBinary);
-
-        return texture;
     }
 }
